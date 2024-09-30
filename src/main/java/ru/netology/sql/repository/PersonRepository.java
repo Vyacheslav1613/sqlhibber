434cf9d8945731271fc743f043f975e09699ca5a
Вячеслав <ldobryakl@yandex.ru>
2024-10-01 02:28:54 +0500

    Del Id

--- src/main/java/ru/netology/sql/repository/PersonRepository.java
+++ src/main/java/ru/netology/sql/repository/PersonRepository.java
@@ -11,13 +11,13 @@ import java.util.List;
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, String> {
 
-//    @Query("SELECT p FROM PersonEntity p WHERE lower(p.cityOfLiving) = lower(:city)")
-//    List<PersonEntity> findAllByCityOfLiving(@Param("city") String city);
+    @Query("SELECT p FROM PersonEntity p WHERE lower(p.cityOfLiving) = lower(:city)")
+    List<PersonEntity> findAllByCityOfLiving(@Param("city") String city);
 
     @Query("SELECT p FROM PersonEntity p WHERE LOWER(p.name) = LOWER(:name)")
     List<PersonEntity> findByName(@Param("name") String name);
 
 
-//    @Query("SELECT p FROM PersonEntity p")
-//    List<PersonEntity> findAll();
+    @Query("SELECT p FROM PersonEntity p")
+    List<PersonEntity> findAll();
 }
